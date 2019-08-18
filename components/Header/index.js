ffc839bf106c9fe214091aa67a6c3a7f535ce626
Leon Chisum <leonchisum3306@gmail.com>
2019-08-18 19:33:59 -0400

    Header Complete

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header(date, temp) {
+    let div = document.createElement('div')// Header
+    div.classList.add('header')
+
+    let span = document.createElement('span')// Date
+    span.classList.add('date')
+    span.innerText = date;
+
+    let h1 = document.createElement('h1') // Title
+    h1.innerText = "Lambda Times"
+
+    let temperature = document.createElement('span')// Weather
+    temperature.classList.add('temp')
+    temperature.innerText = `${temp} \xB0`
+
+    //Append Elements
+    div.appendChild(span)
+    div.appendChild(h1)
+    div.appendChild(temperature)
+
+    return div
+}
+
+let newHeader = Header("March 28, 2019", "98")
+console.log(newHeader)
+let headerContainer = document.querySelector('.header-container')
+headerContainer.appendChild(newHeader)
