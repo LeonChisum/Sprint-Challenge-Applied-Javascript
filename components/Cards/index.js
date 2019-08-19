8d9c5f042c27757db49ee207e712cf016c917342
Leon Chisum <leonchisum3306@gmail.com>
2019-08-18 20:42:40 -0400

    Cards finished, MVP Complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,70 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+            .then(response => {
+                console.log(response.data.articles)
+                let bootstrap = response.data.articles.bootstrap
+                let javascript = response.data.articles.javascript
+                let jquery = response.data.articles.jquery
+                let node = response.data.articles.node
+                let technology = response.data.articles.technology
+
+                bootstrap.forEach(element => {
+                    let newCards = Cards(element)
+                    document.querySelector('.cards-container').appendChild(newCards)
+                })
+
+                javascript.forEach(element => {
+                    let newCards = Cards(element)
+                    document.querySelector('.cards-container').appendChild(newCards)
+                })
+
+                jquery.forEach(element => {
+                    let newCards = Cards(element)
+                    document.querySelector('.cards-container').appendChild(newCards)
+                })
+
+                node.forEach(element => {
+                    let newCards = Cards(element)
+                    document.querySelector('.cards-container').appendChild(newCards)
+                })
+
+                technology.forEach(element => {
+                    let newCards = Cards(element)
+                    document.querySelector('.cards-container').appendChild(newCards)
+                })
+
+            })
+
+
+function Cards(topic){
+    let card = document.createElement('div')
+    card.classList.add('card')
+
+    let headliner = document.createElement('div')
+    headliner.classList.add('headline')
+    headliner.innerText = topic.headline
+
+    let author = document.createElement('div')
+    author.classList.add('author')
+
+    let imageContainer = document.createElement('div')
+    imageContainer.classList.add('img-container')
+
+    let image = document.createElement('img')
+    image.src = topic.authorPhoto
+
+    let span = document.createElement('span')
+    span.innerText = `By: ${topic.authorName}`
+
+    // Appending elements
+    imageContainer.appendChild(image)
+    author.appendChild(imageContainer)
+    author.appendChild(span)
+    card.appendChild(headliner)
+    card.appendChild(author)
+    
+
+    return card
+}
